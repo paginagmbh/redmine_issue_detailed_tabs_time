681d7b3e72b1f4961f2e37ef1e0085dc084e1938
Tobias Fischer <tobias.carl.fischer@gmail.com>
2018-05-22 15:26:58 +0200

    version 0.1.1

--- init.rb
+++ init.rb
@@ -13,6 +13,6 @@ Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/paginagmbh/redmine_issue_detailed_tabs_time'
 end
