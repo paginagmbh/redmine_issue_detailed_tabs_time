f71754d5705ffd830f64640bddfa94a17b42af16
Tobias Fischer <tobias.carl.fischer@gmail.com>
2018-05-22 15:26:39 +0200

    Label changes

--- init.rb
+++ init.rb
@@ -14,4 +14,5 @@ Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
   version '0.1.0'
+  url 'https://github.com/paginagmbh/redmine_issue_detailed_tabs_time'
 end
